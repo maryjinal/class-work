40460a472137ea76b2f2d35a3fdb2b3f0bd4dc80
maryjinal <158549406+maryjinal@users.noreply.github.com>
2024-02-16 09:38:15 +0300

    16.02.2024
    3 задания. савина и бритнер. ибс 235

---
+++ ёжики.py
@@ -0,0 +1,25 @@
+#задача 1 
+a = int(input())
+if a > 0:
+    print("ёжики")
+elif a == 0:
+    print("котики")
+else:
+    print("не ёжики котики")
+#задача 2
+a = int(input())
+if a > 0:
+    print("1")
+elif a == 0:
+    print("0")
+else:
+    print("-1")
+#задача 3
+a = str(input("котик имеет шёрстку?"))
+if a == "да":
+    print("котик пушистый")
+elif a == "нет":
+    print("котик лысый")
+else:
+    print("присмотрись получше")
+    
